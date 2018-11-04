a5c3d4729527f28ba41e9374a0d3076532970b70
Kanahayakoe <44744177+Kanahayakoe@users.noreply.github.com>
2018-11-04 19:41:10 +0700

    Delete comment.js

--- comment.js
+++
@@ -1,1 +0,0 @@
-function downloadJSAtOnload(){var d=document.createElement("script");d.src="URL HOSTING JAVASCRPT DI SINI",document.body.appendChild(d)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
